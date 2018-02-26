0cda31934e090a3caae8a1f35e277b5ba5b5bf5b
Nick Romeo <romeonj@miamioh.edu>
2018-02-26 14:48:51 -0500

    added print statements

--- src/test/java/CSE321/Homework2/AppTest.java
+++ src/test/java/CSE321/Homework2/AppTest.java
@@ -17,16 +17,19 @@ public class AppTest {
 
 	@Test
 	public void TestAdd(){
+		System.out.println("Test Add Run");
 		assertEquals(4, App.add(a, b));
 	}
 
 	@Test
 	public void TestSub(){
+		System.out.println("Test Sub Run");
 		assertEquals(0,App.subtract(a, b));
 	}
 
 	@Test
 	public void TestDiv(){
+		System.out.println("Test Div Run");
 		assertEquals(1,App.divide(a,b));
 	}
 }
