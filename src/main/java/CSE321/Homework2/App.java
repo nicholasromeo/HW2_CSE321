e1ba31a5a69c99bbe9688f2145a53409531187b2
Nick Romeo <romeonj@miamioh.edu>
2018-02-25 21:39:12 -0500

    updated

--- src/main/java/CSE321/Homework2/App.java
+++ src/main/java/CSE321/Homework2/App.java
@@ -1,13 +1,13 @@
 package CSE321.Homework2;
 
 public class App {
-	
+	//added a comment
 	public static int add(int a, int b) {
 		int sum = a + b;
 		return sum;
-		
+
 	}
-	
+
 	public static int subtract(int a, int b) {
 		int sub = a-b;
 		return sub;
